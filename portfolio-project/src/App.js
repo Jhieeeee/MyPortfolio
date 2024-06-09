3656c031b464b76d6028e2267390e7bd48f3718c
REYES <christianrjhie.reyes@cvsu.edu.ph>
2024-06-09 21:26:42 +0800

    React and Bootstrap application

---
+++ portfolio-project/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+function App() {
+  return (
+   <h2>My Personal Portfolio</h2>
+  );
+}
+
+export default App;
